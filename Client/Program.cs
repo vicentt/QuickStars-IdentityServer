7ab02832775a2117934d1d615d8a5601aef5654e
Vicente José Moreno Escobar <vj.moreno@gmail.com>
2018-06-07 17:46:43 +0200

    Para acceder desde casa

---
+++ Client/Program.cs
@@ -0,0 +1,103 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        //static void Main(string[] args) => roMainAsync().GetAwaiter().GetResult();
+        static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error); Console.ReadLine();
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error); Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.ReadLine();
+
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            { Console.WriteLine(response.StatusCode); }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+              
+            }
+
+            Console.ReadLine();
+        }
+
+        private static async Task roMainAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000/");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);Console.ReadLine();
+                return;
+            }
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.Client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("bob", "password", "openid api1 profile");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.ReadLine();
+
+            var HttpClient = new HttpClient();
+            HttpClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await HttpClient.GetAsync("http://localhost:5001/identity");
+            if(!response.IsSuccessStatusCode)
+            { Console.WriteLine(response.StatusCode); }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+
+                await GetClaims(tokenResponse.AccessToken);
+
+
+                Console.ReadLine();
+            }
+        }
+
+       public static async Task GetClaims(string token)
+        {
+            var client = new UserInfoClient("http://localhost:5000/connect/userinfo");
+
+            var response = await client.GetAsync(token);
+            var identity = response.Claims;
+
+            foreach (var claim in response.Claims)
+            {
+                Console.WriteLine("{0}\n {1}", claim.Type, claim.Value);
+            }
+        }
+    }
+}
---
+++ QuickStars-IdentityServer/Config.cs
@@ -0,0 +1,125 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace QuickStars_IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client
+                {
+                    ClientId="client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" },
+                    AlwaysSendClientClaims = true,
+                    Claims = new List<Claim>
+                    {
+                        new Claim("Ids_", "1,2,3")
+                    }
+                },
+                new Client
+                {
+                    ClientId = "ro.Client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AlwaysIncludeUserClaimsInIdToken =true,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        "api1",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    AlwaysSendClientClaims = true,
+                    Claims = new List<Claim>
+                    {
+                        new Claim("Ids_", "1,2,3")
+                    }
+                },
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    //AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    AllowOfflineAccess = true,
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "http://www.alice.com")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password  = "password",
+                    Claims = new []
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "http://www.boby.com")
+                    }
+                }
+            };
+        }
+
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+    }
+}
